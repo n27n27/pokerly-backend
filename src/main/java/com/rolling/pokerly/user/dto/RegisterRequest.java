7bd3ac571a2da97f2882b0ca5937806592789113
n27 <n27kek@knou.ac.kr>
2025-11-19 15:41:55 +0900

    just: 회원가입시 유효성 체크 임시 삭제

--- src/main/java/com/rolling/pokerly/user/dto/RegisterRequest.java
+++ src/main/java/com/rolling/pokerly/user/dto/RegisterRequest.java
@@ -1,15 +1,14 @@
 package com.rolling.pokerly.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class RegisterRequest {
-    @NotBlank @Size(max = 60)
+    @NotBlank
     private String nickname;
 
-    @NotBlank @Size(min = 6, max = 100)
+    @NotBlank
     private String password;
 }
